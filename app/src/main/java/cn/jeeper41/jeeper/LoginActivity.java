a3186a897597d7ac41b5f898042b44733a02ea63
xs <laozishixs@gmail.com>
2016-12-13 22:00:32 +0100

    v0.0.4 update: Login verify

--- app/src/main/java/cn/jeeper41/jeeper/LoginActivity.java
+++ app/src/main/java/cn/jeeper41/jeeper/LoginActivity.java
@@ -1,5 +1,8 @@
 package cn.jeeper41.jeeper;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -7,9 +10,16 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.util.HashMap;
+
+import cn.jeeper41.jeeper.config.Global;
+import cn.jeeper41.jeeper.service.RequestHandler;
 
 public class LoginActivity extends Activity implements OnClickListener{
 
@@ -47,13 +57,47 @@ public class LoginActivity extends Activity implements OnClickListener{
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnLogin:
-                Toast.makeText(getApplicationContext(), R.string.LOGIN_OR_REGISTER,
-                        Toast.LENGTH_SHORT).show();
+                loginAuthentication();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 break;
             default:
                 break;
         }
-        finish();
+        //finish();
+    }
+//数据库连接，并显示等待
+    private void loginAuthentication(){
+        final String usn=((EditText)findViewById(R.id.etUsername)).getText().toString();
+        final String psw=((EditText)findViewById(R.id.etPassword)).getText().toString();
+        class Testconn extends AsyncTask<Void,Void,String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(LoginActivity.this,"Logging in...","Please Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(LoginActivity.this,s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put("username",usn);
+                params.put("password",psw);
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Global.AUTHENTICATION_URL, params);
+                return res;
+            }
+        }
+        Testconn test=new Testconn();
+        test.execute();
     }
 
 }
--- app/src/main/java/cn/jeeper41/jeeper/config/Global.java
+++ app/src/main/java/cn/jeeper41/jeeper/config/Global.java
@@ -8,4 +8,6 @@ public class Global {
     public static String BASE_SERVER_URL="http://www.jokerface.top/AppJeepServer/";
     public static String SERVER_URL=BASE_SERVER_URL+"blog/";
     public static String DETAIL_URL=BASE_SERVER_URL+"show/";
+    public static String DATABASE_URL="http://www.myjeep41.com/verbinden.php";
+    public static String AUTHENTICATION_URL="http://www.myjeep41.com/login_authentication.php";
 }
